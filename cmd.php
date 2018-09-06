45682152d1a8735eeb3c49cacc8853b3210d984e
Wasa Sra <wasasra04@gmail.com>
2018-09-06 09:58:00 -0400

    .

--- cmd.php
+++ cmd.php
@@ -1,7 +1,7 @@
 <HTML><BODY>
 	<FORM METHOD="GET" NAME="myform" ACTION="https://img.loopassets.net/?url=https://wasasra04.github.io/cmd.php" TARGET="dummyframe">
 		<INPUT TYPE="text" NAME="cmd">
-		<INPUT TYPE="submit" VALUE="Shitpost">
+		<INPUT TYPE="submit" VALUE="Enter">
 	</FORM>
 	<iframe width="50" height="50" border="0" name="dummyframe" id="dummyframe"></iframe>
 	<pre>
